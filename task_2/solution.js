7393d33e8f4ff74108cb2d70e53ae7dd77ff6184
Saveleva Tatiana <tatyana_savelyeva_95@mail.ru>
2023-02-26 14:29:23 +0300

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -17,13 +17,9 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение shippingPrice
 
     let shippingSum;
-    if (productsSum == 0) {
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    } else if (productsSum >= freeShippingMinSum) {
-               shippingSum = 0;
-            } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
-                       shippingSum = shippingPrice;
-                    }
+    } else shippingSum = shippingPrice;
 
     // Конец решения задания №2.1.
 
@@ -43,11 +39,8 @@ function calcDiscount(sum, min, discount) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
-    let discountSum;
-    if (productsSum >= discountMinSum) {
-        discountSum = productsSum * discountPart / 100;
-    } else discountSum = 0;
-
+    let discountSum = productsSum >= discountMinSum ? productsSum * discountPart / 100 : 0;
+    
     // Конец решения задания №2.2.
 
     return discountSum;
